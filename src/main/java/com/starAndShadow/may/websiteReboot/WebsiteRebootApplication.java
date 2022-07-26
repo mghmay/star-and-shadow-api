cef08b0f209df106897b8a0ec726f7d966277012
Mathew May <mghmay@gmail.com>
2022-07-26 10:48:56 +0100

    Initial commit

---
+++ src/main/java/com/starAndShadow/may/websiteReboot/WebsiteRebootApplication.java
@@ -0,0 +1,13 @@
+package com.starAndShadow.may.websiteReboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteRebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteRebootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starAndShadow/may/websiteReboot/WebsiteRebootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starAndShadow.may.websiteReboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteRebootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
